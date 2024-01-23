9de80c0b01c807043f78cfa0efe04ee3fb9165a0
seocringe <privetnambogolet@gmail.com>
2024-01-23 16:03:26 +0200

    A duration variable is introduced to control the running time of the loop. We calculate the start_time at the beginning of the script and check the elapsed time in the loop condition. After the loop, we use tar -cJf to create a compressed .tar.xz archive of the OCR output files.

--- 4suppscreenshot.sh
+++ 4suppscreenshot.sh
@@ -1,26 +1,33 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 # Define a counter and base directory for output
 count=0
+duration=9
 output_dir="$HOME/screenshot_ocr_output"
 
 # Create the output directory if it doesn't exist
 mkdir -p "$output_dir"
 
-# Infinite loop, press Ctrl+C to stop
-while true; do
+# Get the start time
+start_time=$(date +%s)
+
+# Loop for the duration
+while [ $(($(date +%s) - start_time)) -lt $duration ]; do
     # Increment the counter
-    ((count++))
+    count=$((count + 1))
 
     # Take a full-screen screenshot
-    gnome-screenshot -f "$output_dir/screenshot_$count.png"
+    gnome-screenshot -f "${output_dir}/screenshot_${count}.png"
 
     # Perform OCR on the screenshot
-    tesseract "$output_dir/screenshot_$count.png" "$output_dir/screenshot_$count" -l eng
+    tesseract "${output_dir}/screenshot_${count}.png" "${output_dir}/screenshot_${count}" -l eng
 
     # Remove the screenshot image to save disk space
-    rm "$output_dir/screenshot_$count.png"
+    rm "${output_dir}/screenshot_${count}.png"
 
     # Wait for 1 second
     sleep 1
 done
+
+# Compress the OCR output files into an archive with xz compression
+tar -cJf "${output_dir}/ocr_output.tar.xz" -C "$output_dir" .
